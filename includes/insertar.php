42a4ecea35f7d402705198c068702aaf26724c87
BorleskineCode <118285010+Borleskine@users.noreply.github.com>
2023-11-10 09:26:20 -0300

    Update insertar.php

--- includes/insertar.php
+++ includes/insertar.php
@@ -1,49 +1,47 @@
 <?php
 
 // Función para validar RUT chileno
-    function validarRut($rut) {
-        if (!preg_match("/^[0-9]+-[0-9kK]{1}$/", $rut)) {
-            return false;
-        }
-            $rut = explode('-', $rut);
-            $dv = $rut[1];
-            $rut = strrev(preg_replace('/\./', '', $rut[0]));
-            $suma = 0;
-            for($i=0;$i<strlen($rut);$i++){
-                $suma += substr($rut,$i,1)*((($i%6)+2));
-            }
-            $resto = $suma%11;
-            $dvEsperado = 11-$resto;
-            $dv = ($dv == 'k' || $dv == 'K')?'10':$dv;
-            if($dvEsperado != $dv) {
-                return false;
-        }
-    return true;
+function validarRut($rut) {
+    if (!preg_match("/^[0-9]+-[0-9kK]{1}$/", $rut)) {
+        return false;
     }
+    $rut = explode('-', $rut);
+    $dv = $rut[1];
+    $rut = strrev(preg_replace('/\./', '', $rut[0]));
+    $suma = 0;
+    for ($i = 0; $i < strlen($rut); $i++) {
+        $suma += substr($rut, $i, 1) * ((($i % 6) + 2));
+    }
+    $resto = $suma % 11;
+    $dvEsperado = 11 - $resto;
+    $dv = ($dv == 'k' || $dv == 'K') ? '10' : $dv;
+    if ($dvEsperado != $dv) {
+        return false;
+    }
+    return true;
+}
 
 include "../conexion.php";
 
-$nombre=$_POST['nombre'];
-$alias=$_POST['alias'];
-$rut=$_POST['rut'];
-$email=$_POST['email'];
-$id_region=$_POST['cbx_region'];
-$id_comuna=$_POST['cbx_comuna'];
-$id_candidato=$_POST['cbx_candidato'];
-$medios=isset($_POST['medios']) ? $_POST['medios'] : array();
+$nombre = $_POST['nombre'];
+$alias = $_POST['alias'];
+$rut = $_POST['rut'];
+$email = $_POST['email'];
+$id_region = $_POST['cbx_region'];
+$id_comuna = $_POST['cbx_comuna'];
+$id_candidato = $_POST['cbx_candidato'];
+$medios = isset($_POST['medios']) ? $_POST['medios'] : array();
 $medios_str = implode(",", $medios);
 
-
-
 // Validación de datos
-if(empty($nombre) || empty($alias) || empty($rut) || empty($email) || $id_region == 0 || $id_comuna == 0 || $id_candidato == 0 || count($medios) < 2){
+if (empty($nombre) || empty($alias) || empty($rut) || empty($email) || $id_region == 0 || $id_comuna == 0 || $id_candidato == 0 || count($medios) < 2) {
     echo "<script> alert('Debe llenar todos los campos');
         location.href = '../index.php';
     </script>";
     exit();
 }
 
-if(strlen($alias) < 5 || !preg_match('/^[a-zA-Z0-9]+$/', $alias)){
+if (strlen($alias) < 5 || !preg_match('/^[a-zA-Z0-9]+$/', $alias)) {
     echo "<script> alert('El Alias debe tener al menos 5 caracteres y contener sólo letras y números');
         location.href = '../index.php';
     </script>";
@@ -51,54 +49,45 @@ if(strlen($alias) < 5 || !preg_match('/^[a-zA-Z0-9]+$/', $alias)){
 }
 
 // Validar RUT
-if(!validarRut($rut)) {
+if (!validarRut($rut)) {
     echo "<script> alert('El rut debe tener digito verificador y sin puntos');
         location.href = '../index.php';
     </script>";
     exit;
 }
 
-if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo "<script> alert('El correo electrónico ingresado no es válido, por favor ingrese un correo en formato válido');
         location.href = '../index.php';
     </script>";
     exit();
 }
 
-$query = "SELECT id_comuna FROM comunas WHERE id_comuna = $id_comuna AND id_region = $id_region";
-$resultado = $mysqli->query($query);
-if($resultado->num_rows == 0){
-    echo "<script> alert('La comuna seleccionada no corresponde a la región seleccionada, por favor seleccione una comuna válida');
-        location.href = '../index.php';
-    </script>";
-    exit();
+// Evitar inyección SQL utilizando consultas preparadas
+$query = "INSERT INTO votacion (nombre, alias, rut, email, region, comuna, candidato, medios) 
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+$stmt = $mysqli->prepare($query);
+
+if (!$stmt) {
+    die("Error en la preparación de la consulta: " . $mysqli->error);
 }
 
-$query = "SELECT rut FROM votacion WHERE rut = '$rut'";
-$resultado = $mysqli->query($query);
-if($resultado->num_rows > 0){
-    echo "<script> alert('Ya existe un voto registrado con el Rut ingresado, por favor revise el rut');
-        location.href = '../index.php';
-    </script>";
-    exit();
-}
+// Enlazar parámetros
+$stmt->bind_param("ssssiiis", $nombre, $alias, $rut, $email, $id_region, $id_comuna, $id_candidato, $medios_str);
 
-$sql = "INSERT INTO votacion (nombre, alias, rut, email, region, comuna, candidato, medios) 
-VALUES ('$nombre','$alias','$rut','$email','$id_region','$id_comuna','$id_candidato','$medios_str')";
-
-$query = mysqli_query($mysqli, $sql);
-
-if($query){
-
+// Ejecutar la consulta
+if ($stmt->execute()) {
     echo "<script> alert('Voto registrado');
         location.href = '../index.php';
     </script>";
-
-}else{
-
+} else {
     echo "<script> alert('El voto no fue ingresado');
         location.href = '../index.php';
     </script>";
 }
 
+// Cerrar la declaración y la conexión
+$stmt->close();
+$mysqli->close();
 ?>
